baf542552cfc60dacb5bf5f134753f8a6de91d94
divir94 <divir94@gmail.com>
2014-04-26 08:23:14 -0500

    -

--- Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
+++ Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
@@ -3,27 +3,33 @@
 	    $('#calendar').fullCalendar({
 
 			dayClick: function() {
-		        $('#calendar').fullCalendar('changeView', 'agendaDay');
+		        //$('#calendar').fullCalendar('changeView', 'agendaDay');
 			},	    
 
 		    eventClick: function(event, element) {
-		        event.title = "CLICKED!";
-		        $('#calendar').fullCalendar('updateEvent', event);
+		        //event.title = "CLICKED!";
+		        //$('#calendar').fullCalendar('updateEvent', event);
 		    },
 
 		    weekends: false,
 		    defaultView: 'agendaWeek',
+		    timeFormat: '',
+		    firstHour: 9,
+		    //minTime: '8:00',
+		    //maxTime: '17:30',
+		    slotEventOverlap: false,
+
 
 			header: {
 				left:   'month agendaDay agendaWeek',
-				center: 'title',
-				right:  'today prev,next'
+				center: '',
+				right:  'prev,next'
 			}
 		
 		});
 
 		var strJSON,
-			newJson = [];
+			newJSON = [];
 
 		function getDate(day) {
 			var day2num = { "Mo": 1, "Tu": 2, "We": 3, "Th": 4, "Fr": 5 };
@@ -32,37 +38,35 @@
 
 		function transformJSON() {
 
-			$.getJSON("http://vazzak2.ci.northwestern.edu/courses/?term=4540&subject=EECS", "json", function(result){
+			$.getJSON("http://vazzak2.ci.northwestern.edu/courses/?term=4540&subject=ECON", "json", function(result){
 				var old = result;
 				var len = old.length,
 				    i;
+				$("#resultarea").append(len);
 
-				$.each(result.slice(0,10), function(i, field){
+				$.each(result.slice(0,40), function(i, field){
 					for (var i=0;i<field["meeting_days"].length;i+=2) {
-						//alert(field["meeting_days"] + " " + field["meeting_days"].slice(i,i+2));
-						newJson.push(
+						newJSON.push(
 							{
 								title: field["subject"] + " " + field["catalog_num"] + ": " +field["title"],
 								start: "2014-09-0" + getDate(field["meeting_days"].slice(i,i+2)) + "T" + field["start_time"] + "Z",
 								end: "2014-09-0" + getDate(field["meeting_days"].slice(i,i+2)) + "T" + field["end_time"] + "Z",
-								//start: field["start_date"] + "T" + field["start_time"] + "+01",
-								//end: field["start_date"] + "T" + field["end_time"] + "+02",
 								allDay: false
 							}
 						);
 					}
 				});
 
-				strJSON = JSON.stringify(newJson, undefined, 2); 
+				strJSON = JSON.stringify(newJSON, undefined, 2); 
 				//$("#resultarea").append(strJSON);
-				test(newJson);
+				displayCourses(newJSON);
 					
 			});
 		};
 
 
-		function test(testJSON) {
-			$('#calendar').fullCalendar('addEventSource', testJSON);
+		function displayCourses(newJSON) {
+			$('#calendar').fullCalendar('addEventSource', newJSON);
 		};
 
 		transformJSON();
