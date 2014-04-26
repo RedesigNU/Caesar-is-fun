4972c90bff86ed16ddcf25cb0b680904e85050c1
divir94 <divir94@gmail.com>
2014-04-26 11:15:28 -0500

    working!

--- Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
+++ Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
@@ -27,25 +27,24 @@
 			numResults = 20,
 			mySubject = "EECS";
 
-		function getDate(day) {
-			var day2num = { "Mo": 1, "Tu": 2, "We": 3, "Th": 4, "Fr": 5 };
-			return day2num[day];
-		};
 
 		function transformJSON() {
 
 			$.getJSON("http://vazzak2.ci.northwestern.edu/courses/?term=4540&subject=" + mySubject, "json", function(result){
 				var old = result;
 					len = old.length;
-				$("#resultarea").append("Number of results: " + len + "<br>");
+				$("#resultarea").append("Number of results: " + len + " Displaying: " 
+				+ numResults + "<br>");
 
 				$.each(result.slice(0,numResults), function(i, field){
-					displayCourseList(field["subject"] + " " + field["catalog_num"] + ": " +field["title"]);
+					var courseTitle = field["subject"] + " " + field["catalog_num"] + ": " +field["title"],
+						courseID = field["class_num"] ;
+					displayCourseList( courseTitle, courseID);
 					for (var i=0;i<field["meeting_days"].length;i+=2) {
 						newJSON.push(
 							{
-								id: field["catalog_num"],
-								title: field["subject"] + " " + field["catalog_num"] + ": " +field["title"],
+								id: courseID,
+								title: courseTitle,
 								start: "2014-09-0" + getDate(field["meeting_days"].slice(i,i+2)) + "T" + field["start_time"] + "Z",
 								end: "2014-09-0" + getDate(field["meeting_days"].slice(i,i+2)) + "T" + field["end_time"] + "Z",
 								allDay: false
@@ -57,22 +56,37 @@
 				strJSON = JSON.stringify(newJSON, undefined, 2); 
 				//$("#resultarea").append(strJSON);
 				displayCourses(newJSON);
+				//removeCourse(37991);
 					
 			});
 		};
 
 
-		function displayCourses(newJSON) {
-			$('#calendar').fullCalendar('addEventSource', newJSON);
+
+		function getDate(day) {
+			var day2num = { "Mo": 1, "Tu": 2, "We": 3, "Th": 4, "Fr": 5 };
+			return day2num[day];
 		};
 
-		function displayCourseList(title) {
-			var html = "<input type='checkbox'>" + title + "<br>";
+		function displayCourses(newJSON) {
+			$('#calendar').fullCalendar('addEventSource', newJSON);
+			$("input[type=checkbox]").change(function () {
+				removeCourse(this.id);
+			});
+		};
+
+		function displayCourseList(title, id) {
+			var html = "<input type='checkbox' " + "id=" + id + " checked='checked'>" + title + "<br>";
 			$('#resultarea').append(html);
+		};
+
+		function removeCourse(id) {
+			$('#calendar').fullCalendar('removeEvents', id);
 		};
 
 		transformJSON();
 		$('#calendar').fullCalendar('gotoDate', 2014, 8, 1);
+
 
 	});
 
