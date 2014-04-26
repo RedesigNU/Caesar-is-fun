3a122cb2ed74eadbe36fe0e9903b50149b9c4558
wtchou712 <wtchou712@gmail.com>
2014-04-26 05:22:29 -0500

    update

--- Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
+++ Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
 
+<<<<<<< HEAD
 	var date = new Date();
 	var d = date.getDate();
 	var m = date.getMonth();
 	var y = date.getFullYear();
+=======
+		var courses = {
+    events: [
+        {
+            title: 'Event1',
+            start: '2011-04-04'
+        },
+        {
+            title: 'Event2',
+            start: '2011-05-05'
+        }
+        // etc...
+    ],
+    color: 'yellow',   // an option!
+    textColor: 'black' // an option!
+}
+		
+	    $('#calendar').fullCalendar({
+>>>>>>> FETCH_HEAD
 
 	var courses = [
 		{
@@ -34,6 +54,7 @@ $(document).ready(function() {
 	    weekends: false,
 	    defaultView: 'agendaWeek',
 
+<<<<<<< HEAD
 		header: {
 			left:   'agendaDay agendaWeek',
 			center: 'title',
@@ -62,6 +83,36 @@ $(document).ready(function() {
 					}
 				);
 				
+=======
+		var strJSON,
+			newJson = {transformedData:[]};
+
+		function transformJSON() {
+
+			$.getJSON("http://vazzak2.ci.northwestern.edu/courses/?term=4540&subject=EECS", "json", function(result){
+				var old = result;
+				var len = old.length,
+				    i;
+
+				$.each(result.slice(0,5), function(i, field){
+
+					newJson.transformedData.push(
+						{
+							title: field["subject"] + " " + field["catalog_num"] + ": " +field["title"],
+							start: new Date(y, m, d-2, 10, 30),
+							end: new Date(y, m, d-2, 11, 30),
+							allDay: false
+						}
+					);
+					
+				});
+
+				strJSON = JSON.stringify(newJson, undefined, 2); 
+
+				$("#resultarea").append(strJSON);
+				test(strJSON);
+					
+>>>>>>> FETCH_HEAD
 			});
 
 			strJSON = JSON.stringify(newJson, undefined, 2); 
@@ -72,6 +123,22 @@ $(document).ready(function() {
 		return newJson;
 	};
 
+<<<<<<< HEAD
 	var res = transformJSON();
 	
 });
+=======
+		function test(testJSON) {
+			$('#calendar').fullCalendar('addEventSource', testJSON);
+		};
+
+		var res = transformJSON();
+		$('#calendar').fullCalendar('addEventSource', courses);
+
+	});
+
+
+
+
+
+>>>>>>> FETCH_HEAD
