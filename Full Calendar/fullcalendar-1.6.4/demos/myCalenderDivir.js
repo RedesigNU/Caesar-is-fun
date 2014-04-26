a4027e119671e7df473ba9e0d752c42a855a99bd
divir94 <divir94@gmail.com>
2014-04-26 05:13:55 -0500

    -

--- Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
+++ Full Calendar/fullcalendar-1.6.4/demos/myCalenderDivir.js
@@ -5,20 +5,21 @@
 		var m = date.getMonth();
 		var y = date.getFullYear();
 
-		var courses = [
-			{
-				id: 213,
-				title: 'EECS 213',
-				start: new Date(y, m, d-1, 10, 30),
-				allDay: false
-			},
-			{
-				id: 214,
-				title: 'EECS 214',
-				start: new Date(y, m, d-2, 11, 00),
-				allDay: false
-			}
-		]
+		var courses = {
+    events: [
+        {
+            title: 'Event1',
+            start: '2011-04-04'
+        },
+        {
+            title: 'Event2',
+            start: '2011-05-05'
+        }
+        // etc...
+    ],
+    color: 'yellow',   // an option!
+    textColor: 'black' // an option!
+}
 		
 	    $('#calendar').fullCalendar({
 
@@ -42,36 +43,49 @@
 		
 		});
 
-		$('#calendar').fullCalendar('addEventSource', courses);
+		var strJSON,
+			newJson = {transformedData:[]};
 
 		function transformJSON() {
-			var strJSON,
-				newJson = {transformedData:[]};
+
 			$.getJSON("http://vazzak2.ci.northwestern.edu/courses/?term=4540&subject=EECS", "json", function(result){
 				var old = result;
 				var len = old.length,
 				    i;
 
-				$.each(result.slice(0,10), function(i, field){
+				$.each(result.slice(0,5), function(i, field){
 
 					newJson.transformedData.push(
 						{
 							title: field["subject"] + " " + field["catalog_num"] + ": " +field["title"],
-							start: new Date(y, m, d, 10, 30),
-							end: new Date(y, m, d, 11, 30)
+							start: new Date(y, m, d-2, 10, 30),
+							end: new Date(y, m, d-2, 11, 30),
+							allDay: false
 						}
 					);
 					
 				});
 
 				strJSON = JSON.stringify(newJson, undefined, 2); 
+
 				$("#resultarea").append(strJSON);
+				test(strJSON);
 					
 			});
 
 			return newJson;
 		};
 
+		function test(testJSON) {
+			$('#calendar').fullCalendar('addEventSource', testJSON);
+		};
+
 		var res = transformJSON();
-		
+		$('#calendar').fullCalendar('addEventSource', courses);
+
 	});
+
+
+
+
+
